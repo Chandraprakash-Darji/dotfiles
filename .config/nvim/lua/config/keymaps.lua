87c81ffd4948443acc0497bcaf044a01c9ba1795
Chandraprakash-Darji <prakashchandra3786@gmail.com>
2025-09-07 10:34:05 +0530

    init

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,6 @@
+local set = vim.keymap.set
+
+set("n", "<leader>rr", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "replace" })
+set("n", "<S-x>", function()
+  Snacks.bufdelete()
+end, { desc = "Delete Buffer" })
---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,5 @@
+local opt = vim.opt
+
+opt.cursorline = false
+opt.sidescrolloff = 16
+opt.scrolloff = 8
